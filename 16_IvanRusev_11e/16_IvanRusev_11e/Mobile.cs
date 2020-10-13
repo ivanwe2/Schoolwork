a3a60283aebc1fdefe852d26816d96edb7ca024f
ivanwe2 <45885771+ivanwe2@users.noreply.github.com>
2020-10-13 14:38:39 +0300

    Add files via upload

---
+++ 16_IvanRusev_11e/16_IvanRusev_11e/Mobile.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_IvanRusev_11e
+{
+    public class Mobile
+    {
+        //class properties
+        private string model;
+        public string Model 
+        {
+            get { return model; }
+            private set { model = value; }
+        }
+
+        private string brand;
+        public string Brand
+        {
+            get { return brand; }
+            private set { brand = value; }
+        }
+
+        private int yearOfManufacture;
+        public int YearOfManufacture
+        {
+            get { return yearOfManufacture; }
+            private set 
+            { 
+                if(value > 2020 || value < 1800)
+                {
+                    throw new ArgumentException("Invalid year");
+                }
+                else yearOfManufacture = value; 
+            }
+        }
+
+        private double kilometers;
+        public double Kilometers
+        {
+            get { return kilometers; }
+            private set 
+            { 
+                if(value < 0)
+                {
+                    throw new ArgumentException("Kilometers cannot be smaller than zero!");
+                }
+                else kilometers = value; 
+            }
+        }
+
+        public Owner MobileOwner { get; private set; }
+        
+        public static int mobilesCount;
+
+        //constructors
+
+        public Mobile()
+        {
+            this.Model = null;
+            this.Brand = null;
+            this.YearOfManufacture = 0;
+            this.Kilometers = 0;
+            this.MobileOwner = new Owner(null,null,null);
+            mobilesCount++;
+        }
+
+        public Mobile(string model, string brand) : this()
+        {
+            this.Model = model;
+            this.Brand = brand;
+        }
+
+        public Mobile(string model, string brand, int year)
+            : this(model, brand)
+        {
+            this.YearOfManufacture = year;
+        }
+
+        public Mobile(string model, string brand, int year, double kms)
+            : this(model, brand, year)
+        {
+            this.Kilometers = kms;
+        }
+
+        public Mobile(string model, string brand, int year, double kms, Owner owner)
+            : this(model, brand, year, kms)
+        {
+            this.MobileOwner = owner;
+        }
+
+        //functionality
+
+        public static void PrintMobileInfo(Mobile m)
+        {
+            Console.WriteLine();
+            Console.WriteLine("###Selected mobile: ###");
+
+            Console.WriteLine("Brand, Model and YoM: {0} {1} , {2}",m.Brand,m.Model,m.YearOfManufacture);
+            Console.WriteLine("Current mileage: {0}",m.Kilometers);
+            Console.WriteLine("Owners info:");
+            Console.WriteLine("-Name: {0} {1}",m.MobileOwner.FirstName, m.MobileOwner.LastName);
+            Console.WriteLine("-Phone number: {0}",m.MobileOwner.PhoneNumber);
+
+        }
+    }
+}
---
+++ 16_IvanRusev_11e/16_IvanRusev_11e/MobileTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_IvanRusev_11e
+{
+    public static class MobileTest
+    {
+        public static void BeginTest()
+        {
+            //begining test
+            Console.WriteLine("How many automobiles do you want to insert in the system? x=>1");
+            int n = int.Parse(Console.ReadLine());
+            if(n >= 1)
+            {
+                // creating a list for the cars
+                List<Mobile> list = new List<Mobile>();
+                for (int i = 0; i < n; i++)
+                {
+                    //initializing cars and owner
+                    
+                    Console.WriteLine("Mobile number {0}",i+1);      
+                    Console.WriteLine("Write mobile model:");
+                    string model = Console.ReadLine();
+
+                    Console.WriteLine("Write mobile brand:");
+                    string brand = Console.ReadLine();
+
+                    Console.WriteLine("Write mobile year:");
+                    int year = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Write mobile kilometers:");
+                    double kms = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Write owner names and phone {firstname, lastname, phoneNumber");
+                    List<string> ownerprop = Console.ReadLine().Split(',').ToList();
+                    Owner o = new Owner(ownerprop[0], ownerprop[1], ownerprop[2]);
+
+                    Mobile m = new Mobile(model, brand, year, kms, o);
+                    list.Add(m);
+                    Console.WriteLine("Mobile {0} added!", i + 1);
+                }
+                //printing info
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Mobile.PrintMobileInfo(list[i]);
+                }
+            }
+            else Console.WriteLine("Have a nice day!");
+
+        }
+    }
+}
